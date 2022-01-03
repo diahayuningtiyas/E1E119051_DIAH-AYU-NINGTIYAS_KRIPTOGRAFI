869b921294b3ef1c88be159a06f5aac90910e73e
diahayuningtiyas <85110891+diahayuningtiyas@users.noreply.github.com>
2022-01-03 22:58:27 +0800

    Add files via upload

---
+++ TUGAS KRIPTOGRAFI.py
@@ -0,0 +1,28 @@
+def encript(plaintext, key):
+    encoded = []
+
+    for i in range(len(plaintext)):
+        xor = ord(plaintext[i]) ^ ord(key[i % len(key)])
+        encoded.append(chr(xor))
+
+    return ''.join(encoded)
+
+def decript(chipertext, key):
+    return encript(chipertext, key)
+
+def main():
+    #fungsi dari enkripsi
+    plaintext = str(input("Masukkan plaintext: "))
+    key = 'ABC'
+
+    print("Plaintext: ", plaintext)
+    print("Enkripsi: ", encript(plaintext, key))
+
+    #fungsi dari dekripsi
+    chipertext = str(input("Masukkan chippertest: "))
+    key = "ABC"
+
+    print("Chipertext: ", chipertext)
+    print("Dekripsi: ", decript(chipertext, key))
+
+main()
